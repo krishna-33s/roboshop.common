d8a4fe464ec5a52c596950512871ab1ecc366b3f
krishna <venkatakrishnaredd450@gmail.com>
2026-04-14 19:08:44 +0530

    set up

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,15 @@ root
 nginx_setup
 
 
+dnf module disable nginx -y &>>$log_file
+Validate $? "disabling nginx"
+
+dnf module enable nginx:1.24 -y &>>$log_file
+Validate $? "enabling:1.24 nginx"
+
+dnf install nginx -y &>>$log_file
+Validate $? "installing nginx"
+
 systemctl enable nginx  &>>$log_file
 systemctl start nginx 
 Validate $? "Enabled and started nginx"
@@ -18,8 +27,6 @@ cd /usr/share/nginx/html &>>$log_file
 
 unzip /tmp/frontend.zip &>>$log_file
 Validate $? "code and unzipping"
-
-rm -rf /etc/nginx/nginx.conf
 
 cp $current_path/nginx.conf /etc/nginx/nginx.conf &>>$log_file
 Validate $? "configuration added"
