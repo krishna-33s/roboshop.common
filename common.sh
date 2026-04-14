24d86752faecfa0ea1a65ec812c1f933de8fc005
krishna <venkatakrishnaredd450@gmail.com>
2026-04-14 13:14:50 +0530

    set up

--- common.sh
+++ common.sh
@@ -15,7 +15,7 @@ echo "$(date "+%Y-%m-%d %H:%M:%S") | script started at: $(date)" | tee -a $log_f
 
 root(){
     if [ $user_id -ne 0 ]; then
-        echo -e "$r pls run the script to root user" | tee -a $log_file
+        echo -e "$r pls run the script to root user $n" | tee -a $log_file
         exit 1
 
     fi
@@ -23,15 +23,15 @@ root(){
 
 Validate(){
     if [ $1 -ne 0 ]; then
-        echo -e "$(date "+%Y-%m-%d %H:%M:%S") |$r $2...failed" | tee -a $log_file
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") |$r $2...failed $n" | tee -a $log_file
         exit 1
     else
-        echo -e "$(date "+%Y-%m-%d %H:%M:%S") |$g $2...success" | tee -a $log_file
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") |$g $2...success $n" | tee -a $log_file
     fi
 }
 
 total_time_taken(){
     end_time=$(date +%s)
     total_time=$(($end_time - $start_time))
-    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | scripts completed in : $g $total_time in seconds" | tee -a $log_file 
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | scripts completed in : $g $total_time in seconds $n" | tee -a $log_file 
 }
